a9c9a40b1af8c667ac9bcb3f97d2b30b5059de5d
Titi <levendeurlillois@hotmail.fr>
2024-10-07 12:07:59 +0200

    ajout des logiques des likes dans le fichier likes : impossible de reliker une personne, impossible de liker > 10 si gratuit, creation d'un channel si deux personnes se like mutuellement

--- routes/likes.js
+++ routes/likes.js
@@ -23,52 +23,54 @@ router.post('/addLikes', async (req,res) => {
        id_user_liked
     } = req.body;
 
-    const checkIfLiked = await checkAlreadyLiked();
+    const checkIfLiked = await checkAlreadyLiked(id_user_liker, id_user_liked);
     const canLike = await checkCanLike(id_user_liker);
 
-    if(checkIfLiked == 0){
+    console.log("checkifliked", checkIfLiked, "canlike", canLike);
+    
+    if(!checkIfLiked){
         return res.status(201).send({message: "impossible de liker de nouveau cette personne"})
     }
-    
-    if(!canLike){
+    else if(!canLike){
         return res.status(201).send({message: "Vous ne pouvez plus liker, pensez à devenir premium pour bénéficier de tous les avantages"})
     }
+    else{
+        const sql =' INSERT INTO likes (id_user_liker, id_user_liked, created_at) VALUE (?,?,?)';
+        db.query(sql,[id_user_liker, id_user_liked, addDate], async (err,result) =>{
+            if (err) {
+                return res.status(500).send(err);
+            }
+            await createChannel(id_user_liker, id_user_liked)
 
-    //console.log(canLike);
-    //console.log("if", ifLikes);
-
-
-    const sql =' INSERT INTO likes id_user_liker, id_user_liked, created_at) VALUE (?,?,?)';
-    db.query(sql,[id_user_liker, id_user_liked, addDate], (err,result) =>{
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.status(201).send({message: 'like envoyé', nombre: ifLikes})
-    })
-    res.status(201).send({nombre: ifLikes})
+            res.status(201).send({message: 'like envoyé'})
+        })
+    }
 })
 
-function checkAlreadyLiked(){
-    let AlreadyLiked;
-    const sql = 'SELECT id_like FROM `likes` WHERE id_user_liker = ? AND id_user_liked = ?;';
+function checkAlreadyLiked(id_user_liker, id_user_liked){
+/* seulement pour les test postman */
+id_user_liker = 1
+id_user_liked = 14
+/* fin test postman */
+
+
+    //const sql = 'SELECT id_like FROM `likes` WHERE id_user_liker = ? AND id_user_liked = ?;';
+    const sql = 'SELECT COUNT(*) AS count FROM likes WHERE id_user_liker = ? AND id_user_liked = ?;';
     return new Promise((resolve, reject) => {
-        db.query(sql, (err, result) => {
-           
+        db.query(sql, [id_user_liker, id_user_liked], (err, result) => {
+            console.log("res", result);
+            
             if (err) {
-                return reject('err', err);
-            }
-            else{
-                if (!isNaN(result)) {
-                    AlreadyLiked = 0
-                    resolve(AlreadyLiked);
-                }
-                else{
-                    AlreadyLiked = result[0].id_like
-                    resolve(AlreadyLiked);
+                return res.status(500).send(err)
+            } else {
+                if (result.length === 0 || result[0].id_like === 0) {
+                    resolve(true); 
+                } else {
+                    resolve(!false); 
                 }
             }
         });
-    });
+    })
 }
 
 function checkCanLike(idUser){
@@ -80,27 +82,49 @@ function checkCanLike(idUser){
         db.query(sql, [dateLikeOfDay, idUser], (err, result) => {
            
             if (err) {
-                return reject('err', err);
+                return res.status(500).send(err)
             }
             else{
                 const numberLike = result[0].count;
-                console.log(numberLike);
-                
                 if (numberLike < 11) {
-                    
-                    resolve(true);
-                }
-                else{
-                 
-                    resolve(false);
+                    resolve(true); 
+                } else {
+                    resolve(false); 
                 }
             }
         });
-    });
+    })
+}
 
-    return dateLikeOfDay
+function createChannel(id_user_liker, id_user_liked){
+    /* seulement pour les test postman */
+    id_user_liker = 3
+    id_user_liked = 1
+    /* fin test postman */
 
-    //const sql = 'SELECT COUNT(created_at) AS count FROM likes WHERE DATE(created_at) = "2024-10-01";'
+
+    const sql = 'SELECT (SELECT COUNT(*) FROM likes WHERE id_user_liker = 3 AND id_user_liked = 1) + (SELECT COUNT(*) FROM likes WHERE id_user_liker = ? AND id_user_liked = ?) AS total_likes;'
+        db.query(sql, [id_user_liker, id_user_liked], (err, result) => {
+            console.log('nomalement 2', result[0].total_likes);
+                       
+            if (err) {
+                return res.status(500).send(err)
+            }
+            else if(result[0].total_likes < 2){
+                return
+            }
+            else if(result[0].total_likes === 2){
+                const randomNumber = Math.floor(Math.random() * (9999999 - 1000000 + 1)) + 1000000;
+
+                const sql =' INSERT INTO channels (name_channel, id_user1, id_user2) VALUE (?,?,?) ';
+                db.query(sql,[randomNumber, id_user_liker, id_user_liked], (err,result) =>{
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    
+                })
+            }
+        })
 }
 
 
